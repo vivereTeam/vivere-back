1f2002a1b823b7750c296f7eb16626fc29ff382b
David Ericson <davidestudofcg@gmail.com>
2025-03-10 17:42:12 -0300

    feat: add user routes to main server file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
+// server.js
 const express = require('express');
 const cors = require('cors');
 const eventoRoutes = require('./routes/eventoRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
@@ -8,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/eventos', eventoRoutes);
+
+app.use('/usuário', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
