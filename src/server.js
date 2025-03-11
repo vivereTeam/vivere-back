15d468da67530d1e22fef399d9b801dbe1642d63
David Ericson <davidestudofcg@gmail.com>
2025-03-10 21:47:48 -0300

    feat: integrate server.test.js and export app/server in server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-// server.js
-module.exports = { app, server };
 const express = require('express');
 const cors = require('cors');
 const eventoRoutes = require('./routes/eventoRoutes');
@@ -11,10 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/eventos', eventoRoutes);
-
 app.use('/usuario', userRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+module.exports = { app, server };
